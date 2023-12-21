8af03e5a1df4dbb2952ea80003d597c996eea416
vincentasa <84776791+vincentasa@users.noreply.github.com>
2023-12-21 19:22:13 +0200

    update enemy.cs and navmesh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public Transform target;
+    public float viewDistance;
+    public float wanderDistance;
     
     NavMeshAgent agent;
     Rigidbody rb;
@@ -15,11 +17,29 @@ public class Enemy : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = target.position;
+        
+        var distance = Vector3.Distance(transform.position, target.position);
+        
+        if (distance < viewDistance)
+        {
+            agent.destination = target.position;
+        }
+        else
+        {
+            if (agent.velocity == Vector3.zero)
+            {
+                var offset = Random.insideUnitSphere * wanderDistance;
+                agent.destination = transform.position + offset;
+            }
+         
+        }
+    
+    
     }
 }
